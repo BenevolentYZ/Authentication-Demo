dbb8e8a7093e803ffc49de8f58fc8c54394b9482
Yutian <yutiancoding@gmail.com>
2022-06-02 15:47:10 -0400

    demo

---
+++ Security/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Security.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        public Credential Credential { get; set; }
+        public void OnGet()
+        {
+            // two way binding
+            //this.Credential = new Credential { Username = "admin" };
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            if (Credential.Username == "admin" && Credential.Password == "password")
+            {
+                // creating the security context
+                var claims = new List<Claim> {
+                    new Claim(ClaimTypes.Name, "admin"),
+                    new Claim(ClaimTypes.Email, "admin@gmail.com"),
+                    new Claim("Department", "HR"),
+                    new Claim("Admin", "true")
+                };
+
+                var identity = new ClaimsIdentity(claims, "MyCookieAuth");
+                ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+                await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
+
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
+        }
+    }
+
+    public class Credential
+    {
+        [Required]
+        [Display(Name = "User name")]
+        public string Username { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
